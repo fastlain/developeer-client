f8c1b09b5900dd088838c0b1516b9a839cd7f828
fastlain <michaeldallain@gmail.com>
2018-10-24 09:02:21 -0700

    Add FeedbackForm component

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import styles from '../css_modules/FeedbackForm.module.css';
+
+import Button from './Button';
+
+class FeedbackForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    submitForm = (e) => {
+        e.preventDefault();
+        // navigate to dashboard
+        this.props.history.push('/main/dashboard');
+    }
+
+    render() {
+
+        const dummyData = {
+            project: 'Example Project',
+            author: 'username123',
+            link: 'https://www.michaelallain.com',
+            questions: [
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            ]
+        };
+
+        const questions = dummyData.questions.map((question, index) => (
+            <fieldset className={styles.questionWrapper} key={index}>
+                <legend>Question {index + 1}:</legend>
+                <label htmlFor={`question${index + 1}`}>
+                    <p>{question}</p>
+                </label>
+                <textarea className={styles.textArea} id={`question${index + 1}`} name={`question${index + 1}`} rows={4}></textarea>
+            </fieldset >
+        ));
+
+        return (
+            <form className={styles.form}>
+                <h3>Project: <span className={styles.innerHeading}>{dummyData.project}</span></h3>
+                <h3 className={styles.formHeading}>Author: <span className={styles.innerHeading}>{dummyData.author}</span></h3>
+                <a className={styles.buttonLink} href={dummyData.link} target="_blank" rel="noopener noreferrer" >VISIT PAGE</a>
+                {questions}
+                <Button type="submit" btnStyle="roomyTopBot" onClick={this.submitForm}>SUBMIT FEEDBACK</Button>
+            </form>
+        );
+    }
+}
+
+export default FeedbackForm;
--- src/components/GiveFeedback.js
+++ src/components/GiveFeedback.js
@@ -4,6 +4,7 @@ import styles from '../css_modules/GiveFeedback.module.css';
 
 import PageTitle from './PageTitle';
 import Button from './Button';
+import FeedbackForm from './FeedbackForm';
 
 class GiveFeedback extends Component {
     constructor(props) {
@@ -18,15 +19,17 @@ class GiveFeedback extends Component {
     }
 
     render() {
-        let instructions;
+        let toRender;
         if (!this.state.started) {
-            instructions = (
+            toRender = (
                 <section className={styles.instructionsWrapper}>
                     <h2>Instructions</h2>
                     <p className={styles.instructions}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <Button type="button" btnStyle="center roomyTopBot" onClick={this.startFeedback}>START</Button>
                 </section>
             );
+        } else {
+            toRender = (<FeedbackForm {...this.props} />);
         }
 
         return (
@@ -35,7 +38,7 @@ class GiveFeedback extends Component {
                     <Button type="button" btnStyle="roomyTopBot">&larr; Dashboard</Button>
                 </Link>
                 <PageTitle>Feedback</PageTitle>
-                {instructions}
+                {toRender}
             </div>
         );
     }
