c9f811e0dee5100cb3bac6a974bc289555816181
fastlain <michaeldallain@gmail.com>
2018-10-25 09:23:19 -0700

    Add 'user' parent object to initialState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,19 @@
 import * as ACTIONS from '../actions';
 
 const initialState = {
-    credit: 0
+    user: {
+        username: "dummy_user",
+        credit: 0,
+        forms: []
+    }
 }
 
 const developeerReducer = (state = initialState, action) => {
     switch (action.type) {
         case ACTIONS.ADD_CREDIT:
-            return Object.assign({}, state, { credit: state.credit + 1 });
+            return Object.assign({}, state, { credit: state.user.credit + 1 });
         case ACTIONS.REMOVE_CREDIT:
-            return Object.assign({}, state, { credit: state.credit - 1 });
+            return Object.assign({}, state, { credit: state.user.credit - 1 });
         default:
             console.warn(`Action '${action}' not recognized.`);
             return state;
