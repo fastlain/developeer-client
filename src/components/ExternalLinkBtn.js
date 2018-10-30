99320c2bff9a8227d86b891fadff6147b7a21fe9
fastlain <michaeldallain@gmail.com>
2018-10-30 12:42:27 -0700

    Add external link icon

--- src/components/ExternalLinkBtn.js
+++ src/components/ExternalLinkBtn.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from '../css_modules/ExternalLinkBtn.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ExternalLinkBtn = (props) => {
     return (
         <a className={styles.buttonLink} href={props.href} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon="external-link-alt" className="FA marginRt" />
             {props.children}
         </a>
     );
