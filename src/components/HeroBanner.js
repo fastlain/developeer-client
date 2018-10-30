629f289a3854e96eb229e936ed32e4915ce0c006
fastlain <michaeldallain@gmail.com>
2018-10-30 11:28:55 -0700

    Add missing semicolon

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../css_modules/HeroBanner.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const HeroBanner = () => {
     return (
