3981ff59836149083c9b9e0e4f577a2be35e5d46
fastlain <michaeldallain@gmail.com>
2018-10-30 10:30:31 -0700

    Simplify props passthrough

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,19 @@ import styles from '../css_modules/Button.module.css';
 
 const Button = (props) => {
 
+    const { btnStyle, ...other } = props;
+
     // extract classNames from corresponding btnStyle prop
     let classes = styles.default;
-    if (props.btnStyle) {
-        const btnStyles = props.btnStyle.split(' ');
-        for (let style of btnStyles) {
+    if (btnStyle) {
+        const styleList = props.btnStyle.split(' ');
+        for (let style of styleList) {
             classes += ` ${styles[style]}`;
         }
     }
 
     return (
-        <button className={classes} type={props.type} onClick={props.onClick}>
+        <button className={classes} {...other}>
             {props.children}
         </button>
     );
