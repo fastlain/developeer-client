b7dd4d483741f562d117d23aeca3d5de82353d1a
fastlain <michaeldallain@gmail.com>
2018-10-30 11:26:39 -0700

    Add arrow icon

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../css_modules/HeroBanner.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const HeroBanner = () => {
     return (
         <div className={styles.heroBanner}>
             <h1>Developeer</h1>
-            <p>Give Feedback - Receive Feedback</p>
+            <p>Give Feedback <FontAwesomeIcon icon="arrows-alt-h" size="lg" style={{ verticalAlign: "-0.4rem" }} /> Receive Feedback</p>
         </div>
     );
 }
