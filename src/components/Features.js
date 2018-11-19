27eb843f5097f888656331a98c28d7baeba04155
fastlain <michaeldallain@gmail.com>
2018-11-19 09:09:05 -0800

    Update featureData

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,25 +3,25 @@ import styles from '../css_modules/Features.module.css';
 
 const Features = () => {
 
-    const dummyFeatureData = [
+    const featureData = [
         {
-            name: "Feature 1",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            screenshot: "https://placeimg.com/250/150/tech"
+            name: 'Custom Feedback Forms',
+            content: 'Easily create forms with instructions and questions for your reviewers to answer.',
+            screenshot: 'https://placeimg.com/250/150/tech'
         },
         {
-            name: "Feature 2",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            screenshot: "https://placeimg.com/250/150/tech"
+            name: 'Equitable Credit System',
+            content: 'Earn credits by reviewing peer projects. Spend credits to receive reviews for your own projects.',
+            screenshot: 'https://placeimg.com/250/150/tech'
         },
         {
-            name: "Feature 3",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            screenshot: "https://placeimg.com/250/150/tech"
+            name: 'Share Anywhere',
+            content: 'Use form links to share forms outside the Developeer community—no credits needed!',
+            screenshot: 'https://placeimg.com/250/150/tech'
         }
     ]
 
-    const features = dummyFeatureData.map((feature, i) => (
+    const features = featureData.map((feature, i) => (
         <div className={styles.feature} key={i}>
             <h2>{feature.name}</h2>
             <p>{feature.content}</p>
