fc290aeca357e65577e2e7a45c88f0e92dc40230
fastlain <michaeldallain@gmail.com>
2018-11-23 13:01:24 -0800

    Add icons, update description and layout of features

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import MainLayout from './MainLayout';
 
 // import fontawesome icons and add to library
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faArrowsAltH, faPlus, faMinus, faTimes, faCopy, faExternalLinkAlt, faLongArrowAltLeft, faEdit, faUser, faStar, faEnvelope, faArrowLeft, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowsAltH, faPlus, faMinus, faTimes, faCopy, faExternalLinkAlt, faLongArrowAltLeft, faEdit, faUser, faStar, faEnvelope, faArrowLeft, faChevronLeft, faPen, faCoins, faShareAlt, faHandsHelping } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-library.add(faArrowsAltH, faPlus, faMinus, faTimes, faCopy, faExternalLinkAlt, faLongArrowAltLeft, faEdit, faUser, faStar, faEnvelope, faLinkedin, faGithub, faArrowLeft, faChevronLeft);
+library.add(faArrowsAltH, faPlus, faMinus, faTimes, faCopy, faExternalLinkAlt, faLongArrowAltLeft, faEdit, faUser, faStar, faEnvelope, faLinkedin, faGithub, faArrowLeft, faChevronLeft, faPen, faCoins, faShareAlt, faHandsHelping);
 
 const App = () => (
     <Router>
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import styles from '../css_modules/Features.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Features = () => {
 
     const featureData = [
         {
-            name: 'Custom Feedback Forms',
-            content: 'Easily create forms with instructions and questions for your reviewers to answer.',
-            screenshot: 'https://placeimg.com/250/150/tech'
+            name: 'Help Fellow Developers',
+            content: 'Review exciting projects shared by your peers. Share your opinions and expertise',
+            icon: 'hands-helping'
         },
         {
-            name: 'Equitable Credit System',
+            name: 'Give and Receive Equitably',
             content: 'Earn credits by reviewing peer projects. Spend credits to receive reviews for your own projects.',
-            screenshot: 'https://placeimg.com/250/150/tech'
+            icon: 'coins'
+        },
+        {
+            name: 'Create Custom Forms',
+            content: 'Easily create feedback forms with instructions and questions for your reviewers to answer.',
+            icon: 'pen'
         },
         {
             name: 'Share Anywhere',
             content: 'Use form links to share forms outside the Developeer community—no credits needed!',
-            screenshot: 'https://placeimg.com/250/150/tech'
+            icon: 'share-alt'
         }
     ]
 
     const features = featureData.map((feature, i) => (
         <div className={styles.feature} key={i}>
-            <h2 className={styles.featureName}>{feature.name}</h2>
-            <p>{feature.content}</p>
-            <img src={feature.screenshot} alt="feature screenshot" />
+            <h2 className={styles.featureTitle}>
+                <FontAwesomeIcon icon={feature.icon} />
+                <span className={styles.featureName}>{feature.name}</span>
+            </h2>
+            <p className={styles.content}>{feature.content}</p>
         </div>
     ));
 
