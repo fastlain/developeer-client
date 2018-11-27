21576dd4bb2474409408b58d32b6c01cffb647a1
fastlain <michaeldallain@gmail.com>
2018-11-27 15:44:11 -0800

    Add surrounding div to fix firefox positioning bug

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,17 +8,19 @@ import Error from './Error';
 class Question extends Component {
     render() {
         return (
-            <fieldset className={styles.question}>
-                <legend className={styles.legend}>Question {this.props.order + 1}:</legend>
-                <label className={styles.blockLabel} htmlFor={`question${this.props.order}`}>
-                    Write your question here:
-                 </label>
-                <textarea id={`question${this.props.order}`} className={styles.textArea} name={`question${this.props.order}`} rows={4} value={this.props.value} onChange={(e) => this.props.setQuestionText(e.target.value, this.props.order)} ref={this.props.qRef} />
-                <Error message={this.props.error} errStyle="textArea" />
-                <Button btnStyle="topRightDelete text" type="button" onClick={() => this.props.deleteQuestion(this.props.order)}>
-                    <FontAwesomeIcon title="Delete Question" icon="times" />
-                </Button>
-            </fieldset>
+            <div className={styles.container}>
+                <fieldset className={styles.question}>
+                    <legend className={styles.legend}>Question {this.props.order + 1}:</legend>
+                    <label className={styles.blockLabel} htmlFor={`question${this.props.order}`}>
+                        Write your question here:
+                     </label>
+                    <textarea id={`question${this.props.order}`} className={styles.textArea} name={`question${this.props.order}`} rows={4} value={this.props.value} onChange={(e) => this.props.setQuestionText(e.target.value, this.props.order)} ref={this.props.qRef} />
+                    <Error message={this.props.error} errStyle="textArea" />
+                    <Button btnStyle="topRightDelete text" type="button" onClick={() => this.props.deleteQuestion(this.props.order)}>
+                        <FontAwesomeIcon title="Delete Question" icon="times" />
+                    </Button>
+                </fieldset>
+            </div >
         );
     }
 }
